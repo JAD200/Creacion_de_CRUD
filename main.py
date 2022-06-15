0fb1b642ca1cbb83959b98c5776091bd905c74db
Juan Di Pasquo <juandipasquo@gmail.com>
2022-06-14 22:15:33 -0300

    Strings operations -- Delete command

--- main.py
+++ main.py
@@ -28,8 +28,17 @@ def list_clients():
     print(f"\nClients list:\n{clients}")
 
 
+def _nonexistent_client(client_name):
+    """_nonexistent_client Message shown in case the client doesn't exists in the list
+
+    Args:
+        client_name (str): Name input by the user
+    """
+    print(f'Client {client_name} is not in the list')
+
+
 def update_client(client_name, updated_client_name):
-    """update_client Changes the name of an existing client with another
+    """update_client Updates the name of an existing client with another
 
     Args:
         client_name (str): Client to be replaced
@@ -40,11 +49,26 @@ def update_client(client_name, updated_client_name):
         clients = clients.replace(client_name, updated_client_name)
         list_clients()
     else:
-        print('Client is not in the list')
+        _nonexistent_client(client_name)
+
+
+def delete_client(client_name):
+    """delete_client Deletes an existing client
+
+    Args:
+        client_name (str): Name of the client to be deleted
+    """
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', '')
+        list_clients()
+    else:
+        _nonexistent_client(client_name)
 
 
 def _print_welcome():
-    print('WELCOME TO Platzi VENTAS\n', '*' * 50)
+    print('WELCOME TO Platzi VENTAS\n' + '*' * 50)
     print('What would you like to do today?')
     print('[C]create client')
     print('[U]update client')
@@ -66,9 +90,11 @@ if __name__ == '__main__':
         list_clients()
     elif command == 'U':
         client_name = _get_client_name()
-        updated_client_name = input('What is the new client name? ').capitalize()
+        updated_client_name = input(
+            'What is the new client name? ').capitalize()
         update_client(client_name, updated_client_name)
     elif command == 'D':
         client_name = _get_client_name()
+        delete_client(client_name)
     else:
         print('Invalid command')
