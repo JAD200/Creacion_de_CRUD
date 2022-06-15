e06031b21a44cff50c085eee008dd65020062339
Juan Di Pasquo <juandipasquo@gmail.com>
2022-06-14 21:53:53 -0300

    Strings operations -- Upadate command

--- main.py
+++ main.py
@@ -28,23 +28,47 @@ def list_clients():
     print(f"\nClients list:\n{clients}")
 
 
+def update_client(client_name, updated_client_name):
+    """update_client Changes the name of an existing client with another
+
+    Args:
+        client_name (str): Client to be replaced
+        updated_client_name (str): New name of the client replaced
+    """
+    global clients
+    if client_name in clients:
+        clients = clients.replace(client_name, updated_client_name)
+        list_clients()
+    else:
+        print('Client is not in the list')
+
+
 def _print_welcome():
     print('WELCOME TO Platzi VENTAS\n', '*' * 50)
     print('What would you like to do today?')
     print('[C]create client')
+    print('[U]update client')
     print('[D]delete client')
+
+
+def _get_client_name():
+    return input('What is the client name? ').capitalize()
 
 
 if __name__ == '__main__':
     _print_welcome()
 
-    command = input()
+    command = input().upper()
 
-    if command == 'C' or command == 'c':
-        client_name = input('Como se llama el usuario a añadir?')
+    if command == 'C':
+        client_name = _get_client_name()
         create_a_client(client_name)
         list_clients()
-    elif command == 'D' or command == 'd':
-        clients_name = input('Como se llama el usuario a borrar?')
+    elif command == 'U':
+        client_name = _get_client_name()
+        updated_client_name = input('What is the new client name? ').capitalize()
+        update_client(client_name, updated_client_name)
+    elif command == 'D':
+        client_name = _get_client_name()
     else:
         print('Invalid command')
