db53103c3c2a6c76703565c5ea39274b6375d8a0
Juan Di Pasquo <juandipasquo@gmail.com>
2022-06-21 19:33:27 -0300

    Challenge -- Make binary search without recursion

--- binary_search.py
+++ binary_search.py
@@ -1,29 +1,32 @@
 import random
 
 
-def binary_search(data, target, low_index, high_index):
+def binary_search(data, target):
     """binary_search Algorithm to search for a number in a large list
 
     Args:
         data (list): List of numbers
         target (int): Number to search
-        low_index (int): Index with the lowest value
-        high_index (int): Index with the highest value
 
     Returns:
         bool: True or False whether the number is or not in the list
     """
-    if low_index > high_index:
-        return False
-# * Establishes the middle of the list
-    mid = (low_index + high_index // 2)
-#   All posible cases for the algorithm
-    if target == data[mid]:
-        return True
-    elif target < data[mid]:
-        return binary_search(data, target, low_index, mid - 1)
-    else:
-        return binary_search(data, target, mid + 1, high_index)
+
+    low_index = 0
+    high_index = len(data) - 1
+
+    while low_index <= high_index:
+        # * Establishes the middle of the list
+        mid = (low_index + high_index) // 2
+        #   All posible cases for the algorithm
+        if data[mid] == target:
+            return True
+        elif target < data[mid]:
+            high_index = mid - 1
+        else:
+            low_index = mid + 1
+
+    return False
 
 
 if __name__ == '__main__':
@@ -33,6 +36,9 @@ if __name__ == '__main__':
     print(data)
 
     target = int(input('What number would you like to find? '))
-    found = binary_search(data, target, 0, len(data) - 1)
+    found = binary_search(data, target)
 
-    print(found)
+    if found:
+        print(f'The number {target} is in the list!!')
+    else:
+        print(f'Sadly, the number {target} isn\'t in the list')
