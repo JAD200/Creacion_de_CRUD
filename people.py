2b966c5abdb36a5509ceb43bd42c3a5d3ed1756f
Juan Di Pasquo <juandipasquo@gmail.com>
2022-06-22 16:27:26 -0300

    OOP -- Object-oriented programming

---
+++ people.py
@@ -0,0 +1,16 @@
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def say_hello(self):
+        print(f'Hello, my name is {self.name} and I am {self.age} years old')
+
+
+if __name__ == '__main__':
+    person = Person('Juan', 21)
+
+    print(f'Age: {person.age}')
+    person.say_hello()
