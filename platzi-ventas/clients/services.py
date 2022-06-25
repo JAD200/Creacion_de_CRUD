28b33fef576e3837c786ce693dbd8886f2a8c103
Juan Di Pasquo <juandipasquo@gmail.com>
2022-06-24 21:33:58 -0300

    API -- Actualization of the client

--- platzi-ventas/clients/services.py
+++ platzi-ventas/clients/services.py
@@ -1,5 +1,5 @@
 import csv
-
+import os
 from clients.models import Client
 
 
@@ -18,3 +18,25 @@ class ClientService:
             reader = csv.DictReader(f, fieldnames=Client.schema())
 
             return list(reader)
+
+    def update_client(self, updated_client):
+        clients = self.list_clients()
+
+        updated_clients = []
+
+        for client in clients:
+            if client[uid] == updated_clients.uid:
+                updated_clients.append(updated_client.to_dict())
+            else:
+                updated_clients.append(client)
+
+        self._save_to_disk(updated_clients)
+
+    def _save_to_disk(self, clients):
+        tmp_table_name = self.table_name + '.tmp'
+        with open(tmp_table_name, 'w') as f:
+            writer = csv.DictWriter(f, fieldnames=Client.schema())
+            writer.writerows(clients)
+
+        os.remove(self.table_name)
+        os.rename(tmp_table_name, self.table_name)
